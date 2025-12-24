8dcae638db2e64e6655d9c816cc670c350a3d43f
Janardhan-Majumder <janardhan.md03@gmail.com>
2025-12-24 19:47:56 +0600

    first commit

---
+++ string_v1.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // char s[10] = "hello";
+    // char s1[10] = "hello2";
+    // s = "gelo"; // wrong
+    // strcpy(s, "Gelo");
+
+    // cout << strcmp(s, s1);
+
+    string s = "Hello";
+    string s2 = "Hello";
+    s = "Hi how are you fjklasjfkldjs";
+    // cout << s;
+    if (s == s2)
+    {
+        /* code */
+    }
+    
+    return 0;
+}
