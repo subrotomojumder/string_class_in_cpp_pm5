d553b0368ed1fe14e16ff278531d962806267c78
Janardhan-Majumder <janardhan.md03@gmail.com>
2026-01-05 22:26:31 +0600

    modifire and iterator practice

--- string_modifires_v4_5.cpp
+++ string_modifires_v4_5.cpp
@@ -15,7 +15,17 @@ int main()
     // s[0] = 'G'; // previous size ke expend korte pare na
 
     // pop_back shese ekta character remove korte pare
-    s.pop_back( );
+    // s.pop_back( );
+
+    // video 5
+    // asign
+    // s = x;
+    // s.assign("Gelo");
+    // s.erase(5, 4); // first p strating and second erase quantity
+    // replace
+    // s.replace(6, 0, "India "); // first strating index, second erase quantity and third replacing text
+    // insert
+    s.insert(6, "Janardhan ");
     cout << s << endl;
     return 0;
 }
